194af892a15f1a4272e6915a07158c5c11a07117
wwinnic1 <wwinnic1@CDV-N10707>
2017-01-19 11:34:22 +0100

    Add project files.

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        List<Pracownik> ListaPracowników = new List<Pracownik>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pracownik abc = new Pracownik(textBox1.Text, textBox2.Text, int.Parse(textBox3.Text), int.Parse(textBox4.Text));
+            ListaPracowników.Add(abc);
+            listBox1.Items.Add(textBox1.Text + " " + textBox2.Text);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string lines = " ";
+            string dir = @"C:\Users\wwinnic1\Documents\test.txt";
+
+            foreach (var Pracownik in ListaPracowników)
+            {
+                lines += $"{Pracownik.imie} {Pracownik.nazwisko} {Pracownik.ID} {Pracownik.LDL}";
+            }
+
+            using (StreamWriter sw1 = new StreamWriter(dir))
+            {
+                sw1.WriteLine(lines);
+                sw1.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string dir = @"C:\Users\wwinnic1\Documents\test.txt";
+
+            using (StreamReader plik = new StreamReader(dir))
+            {
+                string line = plik.ReadToEnd();
+                MessageBox.Show(line + "\n");
+                plik.Close();
+            }
+        }
+    }
+}
+
+// 
---
+++ WindowsFormsApplication2/Pracownik.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class Pracownik
+    {
+       
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+        public int ID { get; set; }
+        public int LDL { get; set; }
+
+        public Pracownik(string imie, string nazwisko, int ID, int LDL)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.ID = ID;
+            this.LDL = LDL;
+
+        }
+
+    }
+}
